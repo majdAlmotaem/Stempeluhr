f33e804279f3ca2cb532dc08c2f35a6cc0c54d0b
majdAlmotaem <majdalmotaem1998@gmail.com>
2024-08-08 15:28:17 +0200

    Tabelle erstellet

--- src/StempelUhr/app.py
+++ src/StempelUhr/app.py
@@ -20,14 +20,18 @@ class stempeluhr(toga.App):
         )
         self.name_input = toga.TextInput(style=Pack(flex=1))
         self.time_label = toga.Label("Aktuelle Zeit: --:--", style=Pack(padding=10))
-        self.in_out_label = toga.Label("Stempel: --", style=Pack(padding=10))
         self.clock_in_button = toga.Button('Kommen', on_press=self.clock_in, style=Pack(padding=10))
         self.clock_out_button = toga.Button('Gehen', on_press=self.clock_out, style=Pack(padding=10))
+
+        # Tabelle erstellen
+        self.table = toga.Table(
+            headings=['Name', 'Datum','Uhrzeit', 'Ein/Aus'], style=Pack(flex=1)
+        )
 
         # Box erstellen und Komponenten hinzufügen
         name_box = toga.Box(children=[name_label, self.name_input], style=Pack(direction=ROW, padding=5))
         button_box = toga.Box(children=[self.clock_in_button, self.clock_out_button], style=Pack(direction=ROW, padding=10))
-        box = toga.Box(children=[self.time_label, name_box, self.in_out_label, button_box],
+        box = toga.Box(children=[self.time_label, name_box, button_box, self.table],
                         style=Pack(direction=COLUMN, alignment=CENTER, padding=10))
 
         # Box zum Hauptfenster hinzufügen
@@ -37,20 +41,32 @@ class stempeluhr(toga.App):
 
         # Uhrzeit regelmäßig aktualisieren
         self.update_time()
+        
 
 
     def update_time(self):
         current_time = datetime.now().strftime("%H:%M")
         self.time_label.text = f"Aktuelle Zeit: {current_time}"
-        
+        self.time_label.refresh()
 
+        
     def clock_in(self, widget):
-        self.in_out_label.text = f"Stempel: {self.name_input.value} hat um {datetime.now().strftime('%H:%M')} Uhr eingestempelt"
-        self.in_out_label.refresh()
+        name = self.name_input.value
+        if name:
+            current_datetime = datetime.now()
+            date = current_datetime.strftime('%Y-%m-%d')
+            time = current_datetime.strftime('%H:%M:%S')
+            self.table.data.append((name, date, time, 'Ein'))   
+            
 
     def clock_out(self, widget):
-        self.in_out_label.text = f"Stempel: {self.name_input.value} hat um {datetime.now().strftime('%H:%M')} Uhr ausgestempelt"
-        self.in_out_label.refresh()
+        name = self.name_input.value
+        if name:
+            current_datetime = datetime.now()
+            date = current_datetime.strftime('%Y-%m-%d')
+            time = current_datetime.strftime('%H:%M:%S')
+            self.table.data.append((name, date, time, 'Aus'))  
+            
 
 
 def main():
